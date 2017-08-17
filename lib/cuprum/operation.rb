9bf9879f303f1416dbd3f805a9bf559b43774c28
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-08-17 17:49:36 -0400

    Implement Cuprum::Operation.

---
+++ lib/cuprum/operation.rb
@@ -0,0 +1,49 @@
+require 'cuprum/function'
+
+module Cuprum
+  # Functional object that with syntactic sugar for tracking the last result.
+  class Operation < Cuprum::Function
+    # @return [Cuprum::Result] The result from the most recent call of the
+    #   operation.
+    attr_reader :result
+
+    # (see Cuprum::Function#call)
+    def call *args, &block
+      @result = nil # Clear reference to most recent result.
+
+      @result = super
+    end # method call
+
+    # @return [Boolean] true if the operation has been called and has a
+    #   reference to the most recent result; otherwise false.
+    def called?
+      !result.nil?
+    end # method called?
+
+    # @return [Array] the errors from the most recent result, or nil if the
+    #   operation has not been called.
+    def errors
+      super || (called? ? result.errors : nil)
+    end # method errors
+
+    # @return [Boolean] true if the most recent result had errors, or false if
+    #   the most recent result had no errors or if the operation has not been
+    #   called.
+    def failure?
+      called? ? result.failure? : false
+    end # method success?
+
+    # @return [Boolean] true if the most recent result had no errors, or false
+    #   if the most recent result had errors or if the operation has not been
+    #   called.
+    def success?
+      called? ? result.success? : false
+    end # method success?
+
+    # @return [Object] the value of the most recent result, or nil if the
+    #   operation has not been called.
+    def value
+      called? ? result.value : nil
+    end # method value
+  end # class
+end # module
