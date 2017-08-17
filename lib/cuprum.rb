d7dc27216cf44a5f4e11bcb4b409a6664cc53930
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-08-17 13:47:30 -0400

    Implement Cuprum#version.

--- lib/cuprum.rb
+++ lib/cuprum.rb
@@ -1,3 +1,8 @@
 # A lightweight, functional-lite toolkit for making business logic a first-class
 # citizen of your application.
-module Cuprum; end
+module Cuprum
+  # @return [String] The current version of the gem.
+  def self.version
+    VERSION
+  end # class method version
+end # module
