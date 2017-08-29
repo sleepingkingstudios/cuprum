0429e7958c462f3bbb1857f4678639c7d822b6bb
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-08-29 15:58:14 -0400

    Implement Result#halt!, #halted?.

--- lib/cuprum/result.rb
+++ lib/cuprum/result.rb
@@ -10,6 +10,7 @@ module Cuprum
     def initialize value = nil, errors: []
       @value  = value
       @errors = errors
+      @halted = false
     end # constructor
 
     # @return [Object] the value returned by calling the function.
@@ -24,6 +25,22 @@ module Cuprum
     def failure?
       !errors.empty?
     end # method failure?
+
+    # Marks the result as halted. Any subsequent chained functions will not be
+    #   run.
+    #
+    # @return [Cuprum::Result] The result.
+    def halt!
+      @halted = true
+
+      self
+    end # method halt!
+
+    # @return [Boolean] true if the function has been halted, and will not run
+    #   any subsequent chained functions.
+    def halted?
+      @halted
+    end # method halted?
 
     # @return [Boolean] true if the function did not generate any errors,
     #   otherwise false.
