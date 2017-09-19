f2bc7498bbf04d4277ae9bc39d670e6fe6d0bd72
Rob Smith <merlin@sleepingkingstudios.com>
2017-09-19 11:54:42 -0400

    Implement BuiltIn::IdentityOperation.

---
+++ lib/cuprum/built_in/identity_operation.rb
@@ -0,0 +1,27 @@
+require 'cuprum/built_in/identity_function'
+require 'cuprum/operation'
+
+module Cuprum::BuiltIn
+  # A predefined operation that returns the value or result it was called with.
+  #
+  # @example With a value.
+  #   operation = IdentityOperation.new.call('custom value')
+  #   operation.value
+  #   #=> 'custom value'
+  #   operation.success?
+  #   #=> true
+  #
+  # @example With a result.
+  #   errors    = ['errors.messages.unknown']
+  #   value     = Cuprum::Result.new('result value', :errors => errors)
+  #   operation = IdentityOperation.new.call(value)
+  #   operation.value
+  #   #=> 'result value'
+  #   operation.success?
+  #   #=> false
+  #   operation.errors
+  #   #=> ['errors.messages.unknown']
+  class IdentityOperation < Cuprum::BuiltIn::IdentityFunction
+    include Cuprum::Operation::Mixin
+  end # class
+end # module
