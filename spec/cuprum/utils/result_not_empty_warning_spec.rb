a0ea1d86d1b48a4ffe7377619c0d8fe2ab18918a
Rob Smith <merlin@sleepingkingstudios.com>
2018-03-31 18:35:49 -0400

    Fix non-empty result warning.
    When #process returns a different result, and the old result has a value but no errors, status or is halted, do not display a warning.

--- spec/cuprum/utils/result_not_empty_warning_spec.rb
+++ spec/cuprum/utils/result_not_empty_warning_spec.rb
@@ -15,7 +15,13 @@ RSpec.describe Cuprum::Utils::ResultNotEmptyWarning do
   describe '#message' do
     let(:expected) { '#process returned a result, but ' }
 
-    include_examples 'should have reader', :message
+    include_examples 'should have reader', :message, ''
+
+    describe 'when the result has a value' do
+      let(:value) { Object.new }
+
+      it { expect(instance.message).to be == '' }
+    end # describe
 
     describe 'when the result has errors' do
       let(:errors) { ['errors.messages.unknown'] }
@@ -66,6 +72,48 @@ RSpec.describe Cuprum::Utils::ResultNotEmptyWarning do
       end # let
 
       it { expect(instance.message).to be == expected }
+    end # describe
+  end # describe
+
+  describe '#warning?' do
+    include_examples 'should have predicate', :warning?, false
+
+    describe 'when the result has a value' do
+      let(:value) { Object.new }
+
+      it { expect(instance.warning?).to be false }
+    end # describe
+
+    describe 'when the result has errors' do
+      let(:errors) { ['errors.messages.unknown'] }
+
+      it { expect(instance.warning?).to be true }
+    end # describe
+
+    describe 'when the result status is set' do
+      let(:result) { super().failure! }
+
+      it { expect(instance.warning?).to be true }
+    end # describe
+
+    describe 'when the result is halted' do
+      let(:result) { super().halt! }
+
+      it { expect(instance.warning?).to be true }
+    end # describe
+
+    describe 'when the result has errors and a set status' do
+      let(:errors) { ['errors.messages.unknown'] }
+      let(:result) { super().success! }
+
+      it { expect(instance.warning?).to be true }
+    end # describe
+
+    describe 'when the result has errors, a set status, and is halted' do
+      let(:errors) { ['errors.messages.unknown'] }
+      let(:result) { super().success!.halt! }
+
+      it { expect(instance.warning?).to be true }
     end # describe
   end # describe
 end # describe
