d85bc6a14ae5cdee4c83d680c44206c41b69a99a
Rob Smith <merlin@sleepingkingstudios.com>
2019-07-24 15:54:04 -0400

    Implement #be_a_passing_result, #be_a_failing_result.

--- lib/cuprum/rspec/be_a_result.rb
+++ lib/cuprum/rspec/be_a_result.rb
@@ -4,6 +4,14 @@ require 'cuprum/rspec/be_a_result_matcher'
 
 module RSpec
   module Matchers # rubocop:disable Style/Documentation
+    def be_a_failing_result
+      be_a_result.with_status(:failure)
+    end
+
+    def be_a_passing_result
+      be_a_result.with_status(:success)
+    end
+
     def be_a_result
       Cuprum::RSpec::BeAResultMatcher.new
     end
--- spec/cuprum/rspec/be_a_result_spec.rb
+++ spec/cuprum/rspec/be_a_result_spec.rb
@@ -5,6 +5,40 @@ require 'cuprum/rspec/be_a_result'
 RSpec.describe RSpec::Matchers do # rubocop:disable RSpec/FilePath
   let(:example_group) { self }
 
+  describe '#be_a_failing_result' do
+    let(:matcher) { example_group.be_a_failing_result }
+
+    it 'should define the method' do
+      expect(example_group)
+        .to respond_to(:be_a_failing_result)
+        .with(0).arguments
+    end
+
+    it { expect(matcher).to be_a Cuprum::RSpec::BeAResultMatcher }
+
+    it 'should set the description' do
+      expect(matcher.description)
+        .to be == 'be a Cuprum result with status: :failure'
+    end
+  end
+
+  describe '#be_a_passing_result' do
+    let(:matcher) { example_group.be_a_passing_result }
+
+    it 'should define the method' do
+      expect(example_group)
+        .to respond_to(:be_a_passing_result)
+        .with(0).arguments
+    end
+
+    it { expect(matcher).to be_a Cuprum::RSpec::BeAResultMatcher }
+
+    it 'should set the description' do
+      expect(matcher.description)
+        .to be == 'be a Cuprum result with status: :success'
+    end
+  end
+
   describe '#be_a_result' do
     let(:matcher) { example_group.be_a_result }
 
