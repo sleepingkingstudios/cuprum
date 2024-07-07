6005bb920c64df644d5823d5616b9a99f147403f
Rob Smith <sleepingkingstudios@gmail.com>
2024-07-07 17:58:24 -0400

    Add flag to reraise handled exceptions.

--- lib/cuprum/exception_handling.rb
+++ lib/cuprum/exception_handling.rb
@@ -5,6 +5,10 @@ require 'cuprum/errors/uncaught_exception'
 module Cuprum
   # Utility module for handling uncaught exceptions in commands.
   #
+  # This functionality can be temporarily disabled by setting the
+  # ENV['CUPRUM_RERAISE_EXCEPTIONS'] flag; this can be used to debug issues when
+  # testing commands.
+  #
   # @example
   #   class UnsafeCommand < Cuprum::Command
   #     private
@@ -39,9 +43,14 @@ module Cuprum
     #   a failing result if a StandardError is raised.
     #
     # @see Cuprum::Processing#call
+    #
+    # @raise StandardError if an exception is raised and the
+    #   ENV['CUPRUM_RERAISE_EXCEPTIONS'] flag is set.
     def call(*args, **kwargs, &block)
       super
     rescue StandardError => exception
+      raise exception if ENV['CUPRUM_RERAISE_EXCEPTIONS']
+
       error = Cuprum::Errors::UncaughtException.new(
         exception: exception,
         message:   "uncaught exception in #{self.class.name} - "
