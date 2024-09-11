7e08d465b39bfd891e9f481be3a9614bc7c183ad
Rob Smith <sleepingkingstudios@gmail.com>
2024-09-10 21:03:21 -0400

    Add missing autoload statements.

--- lib/cuprum.rb
+++ lib/cuprum.rb
@@ -2,20 +2,26 @@
 
 # Toolkit for implementing business logic as function objects.
 module Cuprum
-  autoload :Command,    'cuprum/command'
-  autoload :Error,      'cuprum/error'
-  autoload :MapCommand, 'cuprum/map_command'
-  autoload :Matcher,    'cuprum/matcher'
-  autoload :Middleware, 'cuprum/middleware'
-  autoload :Operation,  'cuprum/operation'
-  autoload :Result,     'cuprum/result'
-  autoload :ResultList, 'cuprum/result_list'
-  autoload :Steps,      'cuprum/steps'
+  autoload :Command,             'cuprum/command'
+  autoload :CommandFactory,      'cuprum/command_factory'
+  autoload :Currying,            'cuprum/currying'
+  autoload :Error,               'cuprum/error'
+  autoload :ExceptionHandling,   'cuprum/exception_handling'
+  autoload :MapCommand,          'cuprum/map_command'
+  autoload :Matcher,             'cuprum/matcher'
+  autoload :Middleware,          'cuprum/middleware'
+  autoload :Operation,           'cuprum/operation'
+  autoload :ParameterValidation, 'cuprum/parameter_validation'
+  autoload :Result,              'cuprum/result'
+  autoload :ResultList,          'cuprum/result_list'
+  autoload :Steps,               'cuprum/steps'
 
   class << self
-    # @return [String] The current version of the gem.
+    # @return [String] the current version of the gem.
     def version
       VERSION
     end
   end
 end
+
+require 'cuprum/version'
