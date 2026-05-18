40f754aa74c8261809f5b73a804c2859e0f6b5b3
Rob Smith <sleepingkingstudios@gmail.com>
2026-05-18 02:30:19 -0400

    Update documentation.

--- lib/cuprum/version.rb
+++ lib/cuprum/version.rb
@@ -10,11 +10,11 @@ module Cuprum
     # Major version.
     MAJOR = 1
     # Minor version.
-    MINOR = 3
+    MINOR = 4
     # Patch version.
-    PATCH = 1
+    PATCH = 0
     # Prerelease version.
-    PRERELEASE = nil
+    PRERELEASE = :alpha
     # Build metadata.
     BUILD = nil
 
